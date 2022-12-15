cd7d137f990c6869666923f7fc1691319589d004
RohitNGiri <procoderpc@gmail.com>
2022-12-15 10:06:04 +0530

    Adding the homePage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Switch>
-
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
 
